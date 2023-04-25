3130d09951768094be94340a0242fc40394436b6
Esteban Pelutiero <estebanpelutiero@gmail.com>
2023-04-25 17:25:34 -0300

    contact section added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,14 +9,45 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className='flex flex-col lg:flex-row'>
           {/* text */}
-          <div className='flex-1'>
+          <motion.div
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once: false, amount: 0.7}}
+          className='flex-1 flex justify-start items-center w-[50%]'>
             <div>
-              <h4>Get in touch</h4>
-              <h2>Let's work <br /> together</h2>
+              <h4 className='text-xl uppercase text-accent font-medium mb-2 tracking-wide'>Get in touch</h4>
+              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Let's work <br /> together</h2>
             </div>
-          </div>
+          </motion.div>
+
           {/* form */}
-          <form className='flex-1'>form</form>
+          <motion.form
+          variants={fadeIn("left", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once: false, amount: 0.7}}
+          className='border rounded-2xl flex flex-col gap-y-6 p-6 items-start lg:w-[40%] lg:mr-28'>
+
+            <input
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
+            type="text"
+            placeholder='Your name'/>
+
+            <input
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
+            type="text"
+            placeholder='Your email'/>
+
+            <textarea
+            className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all mb-12 resize-none'
+            placeholder='Your message'
+            >
+            </textarea>
+
+            <button className='btn btn-lg'>Send message</button>
+
+          </motion.form>
         </div>
       </div>
     </section>
